7ee4cab79c804c74fffa4fcc43962d5a1fc0ef4a
Miguel Castillo <migramcastillo@gmail.com>
2019-06-06 09:32:51 -0500

    Readme and implementation of Tailwind on components

--- components/nav.js
+++ components/nav.js
@@ -1,58 +1,25 @@
 import React from 'react'
 import Link from 'next/link'
 
-const links = [
-  { href: 'https://github.com/segmentio/create-next-app', label: 'Github' }
-].map(link => {
-  link.key = `nav-link-${link.href}-${link.label}`
-  return link
-})
-
 const Nav = () => (
   <nav>
-    <ul>
-      <li>
-        <Link prefetch href="/">
-          <a>Home</a>
+    <ul className="flex justify-between p-2">
+      <li className="font-semibold bg-blue-500 py-2 px-4 text-white rounded hover:bg-blue-600 cursor-pointer">
+        <Link href="https://tailwindcss.com/">
+          <a target="_blank">Tailwind Docs</a>
         </Link>
       </li>
-      <ul>
-        {links.map(({ key, href, label }) => (
-          <li key={key}>
-            <Link href={href}>
-              <a>{label}</a>
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <li className="font-semibold bg-blue-500 py-2 px-4 text-white rounded hover:bg-blue-600 cursor-pointer">
+        <Link href="https://www.purgecss.com/">
+          <a target="_blank">Purge CSS Docs</a>
+        </Link>
+      </li>
+      <li className="font-semibold bg-blue-500 py-2 px-4 text-white rounded hover:bg-blue-600 cursor-pointer">
+        <Link href="https://sass-lang.com/documentation">
+          <a target="_blank">Sass Docs</a>
+        </Link>
+      </li>
     </ul>
-
-    <style jsx>{`
-      :global(body) {
-        margin: 0;
-        font-family: -apple-system, BlinkMacSystemFont, Avenir Next, Avenir,
-          Helvetica, sans-serif;
-      }
-      nav {
-        text-align: center;
-      }
-      ul {
-        display: flex;
-        justify-content: space-between;
-      }
-      nav > ul {
-        padding: 4px 16px;
-      }
-      li {
-        display: flex;
-        padding: 6px 8px;
-      }
-      a {
-        color: #067df7;
-        text-decoration: none;
-        font-size: 13px;
-      }
-    `}</style>
   </nav>
 )
 
--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
 const withSass = require('@zeit/next-sass');
+const withPurgeCss = require('next-purgecss');
 
-module.exports = withSass({
-  webpack: config => {
-    // Fixes npm packages that depend on `fs` module
-    config.node = {
-      fs: 'empty'
-    }
-
-    return config;
-  }
-});
+module.exports = withSass(withPurgeCss({
+  purgeCssPaths: [
+    'pages/**/*',
+    'components/**/*'
+  ]
+}));
